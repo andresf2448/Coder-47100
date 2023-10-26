d9f91671f07d64a174dd3e7471f7d3c38e714dce
andresf2448 <andresf2448@gmail.com>
2023-10-26 17:37:17 -0500

    Clase 11

---
+++ Clase 11/main.js
@@ -0,0 +1,48 @@
+//STORAGE
+//localStorage
+
+// console.log(localStorage);
+// localStorage.setItem("nombre", "andres"); //para guardar en el storage
+// localStorage.setItem("numero", 5);
+// localStorage.setItem("valor", "true");
+
+// let nombre = localStorage.getItem("nombre"); //para traer datos del storage
+// console.log(nombre);
+
+// let numero = localStorage.getItem("numero");
+// console.log(numero);
+
+// let valor = localStorage.getItem("valor");
+// console.log(valor);
+
+// for(let i = 0; i < localStorage.length; i++){
+//   let clave = localStorage.key(i);
+//   console.log(clave);
+//   console.log("Valor asociado", localStorage.getItem(clave));
+// }
+
+// localStorage.removeItem("nombre"); //Elimina por clave
+// localStorage.clear(); //elimina todo el storage
+
+//sessionStorage
+// sessionStorage.setItem("nombre", "andres"); //para guardar en el storage
+// sessionStorage.setItem("numero", 5);
+// sessionStorage.setItem("valor", "true");
+
+// let nombre = sessionStorage.getItem("nombre"); //para traer datos del storage
+// console.log(nombre);
+
+// let numero = sessionStorage.getItem("numero");
+// console.log(numero);
+
+// let valor = sessionStorage.getItem("valor");
+// console.log(valor);
+
+// for(let i = 0; i < sessionStorage.length; i++){
+//   let clave = sessionStorage.key(i);
+//   console.log(clave);
+//   console.log("Valor asociado", sessionStorage.getItem(clave));
+// }
+
+// sessionStorage.removeItem("nombre"); //Elimina por clave
+// sessionStorage.clear(); //elimina todo el storage
