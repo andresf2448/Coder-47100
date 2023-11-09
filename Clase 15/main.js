127165306925ed1c3dbb7d559d2a9745e05280b7
andresf2448 <andresf2448@gmail.com>
2023-11-09 18:42:02 -0500

    Clase 15

--- Clase 15/main.js
+++ Clase 15/main.js
@@ -68,13 +68,81 @@
 //   console.log("Entramos");
 // }, 1000);
 
-let counter = 0;
-const interval = setInterval(() => {
-  counter++;
-  console.log("Counter: ", counter);
+// let counter = 0;
+// const interval = setInterval(() => {
+//   counter++;
+//   console.log("Counter: ", counter);
 
-  if (counter >= 5) {
-    clearInterval(interval);
-    console.log("Se removió el intervalo");
-  }
-}, 1000);
+//   if (counter >= 5) {
+//     clearInterval(interval);
+//     console.log("Se removió el intervalo");
+//   }
+// }, 1000);
+
+// for (let letra of "Mundo") {
+//   setInterval(() => {
+//     console.log(letra);
+//   }, 1000);
+// }
+
+//Promesas
+/* 
+new Promise((resolve, reject) => {
+  cuerpo de la promesa
+})
+*/
+
+// const futuro = (valor) => {
+//   return new Promise((resolve, reject) => {
+//     // if (valor) {
+//     //   resolve("Promesa resuelta");
+//     // }else{
+//     //   reject("Promesa rechazada");
+//     // }
+//     setTimeout(() => {
+//       valor
+//         ? resolve(["camisa", "gorra", "media"])
+//         : reject("Promesa rechazada");
+//     }, 3000);
+//   });
+// };
+
+// //then y el catch
+// futuro(true)
+//   .then((response) => console.log(response))
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log("Proceso finalizado"));
+
+///////////////////////////////////////////////////////////////////////// SERVIDOR
+const BD = [
+  { id: 1, nombre: "pantalon", precio: 1000 },
+  { id: 2, nombre: "zapato", precio: 400 },
+  { id: 3, nombre: "gorra", precio: 370 },
+  { id: 4, nombre: "camisa", precio: 850 },
+];
+
+const traerProductos = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(BD);
+    }, 3000);
+  });
+};
+/////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////// FRONT
+traerProductos()
+  .then((response) => {
+    response.forEach((producto) => {
+      let div = document.createElement("div");
+      div.innerHTML = `
+        <h2>ID: ${producto.id}</h2>      
+        <p>Nombre: ${producto.nombre}</p>
+        <b>$${producto.precio}</b>
+      `;
+
+      document.body.append(div);
+    });
+  })
+  .catch((error) => console.log(error));
+/////////////////////////////////////////////////////////////////////////
